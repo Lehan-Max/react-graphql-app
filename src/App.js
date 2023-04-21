5d5919603045b0df49dea8f0f4296815acd6ad9b
Lehan Max <lehanmx14@gmail.com>
2023-04-21 21:42:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import * as Constants from './constants';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState({ todos: [] })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const queryResult = await axios.post(
+        Constants.GRAPHQL_API, {
+          query: Constants.QUERY
+        }
+      )  
+      
+      const { todos } = queryResult.data.data;
+      console.log(queryResult.data.data)
+      setData({ todos })
+    }
+    fetchData();
+  })
+
+  const renderDone = (done) => {
+    if (done)
+      return <p>True Returned</p>;
+    return <p>False Returned</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LIST OF TODOS</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.todos.map(todo => (
+            <tr key={todo.test}>
+              <td>{todo.id}</td>
+              <td>{todo.description}</td>
+              <td>{renderDone(todo.done)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const GRAPHQL_API = "https://api.mocki.io/v2/c4d7a195/graphql";
+
+export const QUERY = `
+{
+  todos{
+    description
+    done
+    id
+  }
+}
+`;
